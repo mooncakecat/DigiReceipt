3148cf1de4b49f965c307f4f63bb6355ecd024d2
Amy <xlin504@aucklanduni.ac.nz>
2017-04-02 19:39:30 +1200

    Formatting

--- app/src/main/java/com/compsci702/DigiReceipt/ui/receipts/DRViewReceiptsFragment.java
+++ app/src/main/java/com/compsci702/DigiReceipt/ui/receipts/DRViewReceiptsFragment.java
@@ -1,6 +1,7 @@
 package com.compsci702.DigiReceipt.ui.receipts;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,6 +13,7 @@ import com.compsci702.DigiReceipt.R;
 import com.compsci702.DigiReceipt.ui.base.DRBaseFragment;
 import com.compsci702.DigiReceipt.ui.model.DRReceipt;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,82 +23,92 @@ import butterknife.BindView;
  * Display a list of digitalised receipts
  */
 public class DRViewReceiptsFragment extends DRBaseFragment<DRViewReceiptsFragment.FragmentListener> implements
-        DRReceiptsRecyclerViewAdapter.AdapterListener {
+		DRReceiptsRecyclerViewAdapter.AdapterListener {
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    * FragmentListener
    * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-    public interface FragmentListener {
-    }
+	public interface FragmentListener {
+		void onReceiptSelected(String receiptFilename);
+	}
 
-    private final int GRID_COLUMNS = 2;
+	private static final int GRID_COLUMNS = 2;
 
-    private DRReceiptsRecyclerViewAdapter mAdapter;
-    private final List<DRReceipt> mReceipts = new ArrayList<>();
+	private DRReceiptsRecyclerViewAdapter mAdapter;
+	private final List<DRReceipt> mReceipts = new ArrayList<>();
 
-    @BindView(R.id.recyclerview) RecyclerView mRecyclerView;
+	@BindView(R.id.recyclerview) RecyclerView mRecyclerView;
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    * new instance
    * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-    public static DRViewReceiptsFragment newInstance() {
-        DRViewReceiptsFragment viewReceiptFragment = new DRViewReceiptsFragment();
-        return viewReceiptFragment;
-    }
+	public static DRViewReceiptsFragment newInstance() {
+		DRViewReceiptsFragment viewReceiptFragment = new DRViewReceiptsFragment();
+		return viewReceiptFragment;
+	}
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    * life cycle
    * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-    @Override protected View getContentView(LayoutInflater inflater, ViewGroup container) {
-        return inflater.inflate(R.layout.fragment_view_receipts, container, false);
-    }
+	@Override protected View getContentView(LayoutInflater inflater, ViewGroup container) {
+		return inflater.inflate(R.layout.fragment_view_receipts, container, false);
+	}
 
-    @Override protected void onViewCreated(Bundle savedInstanceState) {
-      // FIXME: 3/31/2017 Add dummy data
-      DRReceipt receipt = new DRReceipt() {
-        @Override public int getId() {
-          return 0;
-        }
+	@Override protected void onViewCreated(Bundle savedInstanceState) {
 
-        @Override public String getFilename() {
-          return "sample.png";
-        }
-      };
+		// FIXME: 4/2/2017 Should be done on a new thread
+		String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/DigiReceipt";
+		File directory = new File(path);
+		final File[] files = directory.listFiles();
 
-      mReceipts.add(receipt);
+		// reverse order to show latest receipts at the top
+		for (int i = files.length - 1; i > -1; i--) {
+			final String filePath = files[i].getAbsolutePath();
+			DRReceipt receipt = new DRReceipt() {
+				@Override public int getId() {
+					return 0;
+				}
 
-        mAdapter = new DRReceiptsRecyclerViewAdapter(getActivity(), mReceipts, this);
+				@Override public String getFilename() {
+					return filePath;
+				}
+			};
+			mReceipts.add(receipt);
+		}
 
-        final GridLayoutManager layoutManager = new GridLayoutManager(getContext(), GRID_COLUMNS);
-        mRecyclerView.setLayoutManager(layoutManager);
-        mRecyclerView.setAdapter(mAdapter);
-        mRecyclerView.setHasFixedSize(true);
-    }
+		mAdapter = new DRReceiptsRecyclerViewAdapter(getActivity(), mReceipts, this);
+		mAdapter.notifyDataSetChanged();
 
-    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-      * DRREceiptsRecyclerViewAdapater.AdapterListener
+		final GridLayoutManager layoutManager = new GridLayoutManager(getContext(), GRID_COLUMNS);
+		mRecyclerView.setLayoutManager(layoutManager);
+		mRecyclerView.setAdapter(mAdapter);
+		mRecyclerView.setHasFixedSize(true);
+	}
+
+	/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	  * DRReceiptsRecyclerViewAdapter.AdapterListener
       * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-    @Override public void onReceiptSelected() {
-        // FIXME: 30/03/2017
-    }
+	@Override public void onReceiptSelected(String receiptFilename) {
+		mListener.onReceiptSelected(receiptFilename);
+	}
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    * base class overrides
    * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-    @NonNull @Override protected Class getFragmentListenerClass() {
-        return FragmentListener.class;
-    }
+	@NonNull @Override protected Class getFragmentListenerClass() {
+		return FragmentListener.class;
+	}
 
-    @Override protected void updateView() {
+	@Override protected void updateView() {
 
-    }
+	}
 
-    @NonNull @Override public String getScreenTitle() {
-        return getString(R.string.view_receipts);
-    }
+	@NonNull @Override public String getScreenTitle() {
+		return getString(R.string.view_receipts);
+	}
 }
