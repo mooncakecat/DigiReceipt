05d9331801919397d2f4d65c83e930f3b57488dd
Xizhe Qiu <xqiu318@aucklanduni.ac.nz>
2017-04-20 16:13:31 +1200

    get rid of placeholder
    get rid of placeholder

--- app/src/main/java/com/compsci702/DigiReceipt/ui/model/DRReceiptTemp.java
+++ app/src/main/java/com/compsci702/DigiReceipt/ui/model/DRReceiptTemp.java
@@ -7,7 +7,8 @@ package com.compsci702.DigiReceipt.ui.model;
 public class DRReceiptTemp {
 
     //text is placeholder only since I don't have an actual phone to take photos of receipts
-    private String text = "BananaOrange apple";
+    //private String text = "BananaOrange apple";
+    private String text = "";
 
     public DRReceiptTemp (){
 
