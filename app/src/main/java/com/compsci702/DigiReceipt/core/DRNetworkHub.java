89b52a3d805e1e61890362c38f03cbe88e007940
andy-j92 <andyjang0530@gmail.com>
2017-04-28 23:31:03 +1200

    obfuscated DRNetworkHub

--- app/src/main/java/com/compsci702/DigiReceipt/core/DRNetworkHub.java
+++ app/src/main/java/com/compsci702/DigiReceipt/core/DRNetworkHub.java
@@ -41,11 +41,29 @@ public class DRNetworkHub {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if (fileInputStream != null) {
-                try {
-                    fileInputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+            String val = "asdfasdf";
+            boolean go = true;
+            while(go) {
+                switch (val) {
+                    case "asdfasdf":
+                        if(fileInputStream != null)
+                            val = "fdsafdsa";
+                        else
+                            val = "qwerqwer";
+                        break;
+                    case "qwerqwer":
+                        go = false;
+                        break;
+                    case "fdsafdsa":
+                        try {
+                            fileInputStream.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        val = "qwerqwer";
+                        break;
+                    default:
+                        break;
                 }
             }
         }
@@ -68,25 +86,119 @@ public class DRNetworkHub {
                         +"}], \"image\": {\"content\": \"" + Base64.encodeBase64String(fileContent)+ "\"}}]}");
         httpRequestBodyWriter.close();
         String response = httpConnection.getResponseMessage();
-        if (httpConnection.getInputStream() == null && !response.equals("OK")) {
-            return null;
+
+        String value = "qazqaz";
+        boolean whileLoop = true;
+        while(whileLoop) {
+            switch (value) {
+                case "qazqaz":
+                    if(httpConnection.getInputStream() == null)
+                        value = "qazwsx";
+                    else
+                        value = "wsxwsx";
+                    break;
+                case "wsxwsx":
+                    whileLoop = false;
+                    break;
+                case "qazwsx":
+                    if(!response.equals("OK"))
+                        value = "qwerqwer";
+                    else
+                        value = "wsxwsx";
+                    break;
+                case "qwerqwer":
+                    return null;
+                default:
+                    break;
+            }
         }
+
         Scanner httpResponseScanner = new Scanner (httpConnection.getInputStream());
         String temp = "";
         while (httpResponseScanner.hasNext()) {
             String line = httpResponseScanner.nextLine();
-            if(line.contains("text")){
-                temp = line;
+            value = "condition1";
+            whileLoop = true;
+            while(whileLoop) {
+                switch (value) {
+                    case "condition1":
+                        if(line.contains("text"))
+                            value = "condition2";
+                        else
+                            value = "condition3";
+                        break;
+                    case "condition2":
+                        temp = line;
+                        value = "condition3";
+                        break;
+                    case "condition3":
+                        whileLoop = false;
+                        break;
+                    default:
+                        break;
+                }
             }
         }
+
         String[] tempArray = temp.split(":");
-        if(tempArray.length > 1 && temp.contains("text")){
-            temp = "";
-            for(int i = 1; i < tempArray.length; i++ ){
-               temp += tempArray[i];
+        value = "hellothere";
+        whileLoop = true;
+        while(whileLoop) {
+            switch (value) {
+                case "checknextcondition":
+                    if(temp.contains("text")) {
+                        temp = "";
+                        value = "gotoforloop";
+                    }
+                    else
+                        value = "endwhileloop";
+                    break;
+                case "gotoforloop":
+                    String var = "init";
+                    int i = 1;
+                    boolean bool = true;
+                    while(bool) {
+                        switch (var) {
+                            case "init":
+                                if(i < tempArray.length)
+                                    var = "processArray";
+                                else
+                                    var = "endloop";
+                                break;
+                            case "endloop":
+                                bool = false;
+                                break;
+                            case "processArray":
+                                temp += tempArray[i];
+                                var = "incrementi";
+                                break;
+                            case "incrementi":
+                                i++;
+                                var = "init";
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    value = "processTemp";
+                    break;
+                case "hellothere":
+                    if(tempArray.length > 1)
+                        value = "checknextcondition";
+                    else
+                        value = "endwhileloop";
+                    break;
+                case "endwhileloop":
+                    whileLoop = false;
+                    break;
+                case "processTemp":
+                    temp = temp.replaceAll("\"", "");
+                    temp = temp.trim();
+                    value = "endwhileloop";
+                    break;
+                default:
+                    break;
             }
-            temp = temp.replaceAll("\"", "");
-            temp = temp.trim();
         }
         httpResponseScanner.close();
         return new DRReceiptTemp(temp);
